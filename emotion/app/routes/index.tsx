2f249c1866d95a0e11846e28d3fbe72cc61ac722
Barry May <5514034+barrymay@users.noreply.github.com>
2023-05-14 21:57:55 -0400

    Update packages to latest remix and react

--- emotion/app/routes/index.tsx
+++ emotion/app/routes/index.tsx
@@ -1,6 +1,11 @@
 import styled from "@emotion/styled";
-import { Link } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
 
+export const loader = () => {
+  return json({ message: "Hello, Barry - Server 12345!" });
+}
 const Container = styled("div")`
   font-family: "system-ui, sans-serif";
   line-height: 1.4;
@@ -8,17 +13,20 @@ const Container = styled("div")`
 `;
 
 export default function Index() {
+  const data = useLoaderData();
+  const [state, setState] = useState(0);
   return (
     <Container>
-      <h1>Welcome to Remix with Emotion Example</h1>
+      <h1>Welcome to Remix (Client: 123) with Emotion Example: {data.message}</h1>
       <ul>
         <li>
-          <Link to="/jokes">Jokes</Link>
+          <Link to="/jokes">Jokes {state}</Link>
         </li>
         <li>
           <Link to="/jokes-error">Jokes: Error</Link>
         </li>
       </ul>
+      <button onClick={() => setState(state + 1)}>Click me: {state}</button>
     </Container>
   );
 }
